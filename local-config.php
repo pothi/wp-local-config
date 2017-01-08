29da76b66bd0abdd573c431a007905b598d140de
Pothi Kalimuthu <pothi@tinywp.com>
2017-01-08 19:47:32 +0530

    First commit

---
+++ local-config.php
@@ -0,0 +1,65 @@
+<?php
+
+/*** Variables ***/
+
+// please update the following to fit the actual requirement
+define('DB_NAME', 'db_name');
+define('DB_USER', 'db_user');
+define('DB_PASSWORD', 'db_password');
+define('DB_HOST', 'localhost');
+
+/* End of Variables */
+
+/*
+ * You needn't edit any of the following lines.
+ * Still good to know / understand what's going on
+ */
+
+$wplt_domain = $_SERVER['HTTP_HOST'];
+// for Browser Sync
+// $wplt_domain = "$_SERVER['HTTP_HOST']:2000";
+
+define('WPLT_ROBOTS', 'noindex');
+
+$wplt_protocol = 'http://';
+if (isset($_SERVER['HTTPS']) &&
+    ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+  $wplt_protocol = 'https://';
+}
+
+define('WP_HOME', $wplt_protocol . $wplt_domain);
+define('WP_SITEURL', $wplt_protocol . $wplt_domain);
+
+/* deactivate a set of plugins => https://github.com/joeguilmette/wp-local-toolbox#disable-plugins
+ * - caching plugins
+ * - SEO related plugins
+ * - backup plugins
+ * - analytics plugins
+ * - security plugins
+ * - any other specific plugins
+ */
+
+define('WPLT_DISABLED_PLUGINS', serialize(
+    array(
+        'wp-super-cache/wp-cache.php',
+        'w3-total-cache/w3-total-cache.php',
+        'varnish-http-purge/varnish-http-purge.php',
+        
+        'google-sitemap-generator/sitemap.php',
+        'bwp-google-xml-sitemaps/bwp-simple-gxs.php',
+        'html-sitemap/html-sitemap.php',
+
+        'wordpress-seo/wp-seo.php',
+        'autodescription/autodescription.php',
+        
+        'backup/backup.php',
+        'backupwordpress/backupwordpress.php',
+        'updraftplus/updraftplus.php',
+        
+        'wp-piwik/wp-piwik.php',
+
+        'simple-history/index.php'
+    )
+));
---
+++ wp-config.php
@@ -0,0 +1,16 @@
+<?php
+
+if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
+    # define( 'WP_LOCAL_DEV', true );
+    define( 'WPLT_SERVER', 'dev' );
+    include( dirname( __FILE__ ) . '/local-config.php' );
+} else if ( file_exists( dirname( __FILE__ ) . '/../local-config.php' ) ) {
+    # define( 'WP_LOCAL_DEV', true );
+    define( 'WPLT_SERVER', 'dev' );
+    include( dirname( __FILE__ ) . '/../local-config.php' );
+} else {
+    define('DB_NAME', 'prod_db_name');
+    define('DB_USER', 'prod_db_user');
+    define('DB_PASSWORD', 'prod_db_password');
+    define('DB_HOST', 'prod_db_host');
+}
