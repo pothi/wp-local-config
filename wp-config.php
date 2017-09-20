8269139a97f8b3ad94d32475c8bf186a8ba1478d
Pothi Kalimuthu <pothi@tinywp.com>
2017-09-20 17:28:21 +0530

    Do not search for local-config in multiple locations

--- wp-config.php
+++ wp-config.php
@@ -4,10 +4,6 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
     # define( 'WP_LOCAL_DEV', true );
     define( 'WPLT_SERVER', 'dev' );
     include( dirname( __FILE__ ) . '/local-config.php' );
-} else if ( file_exists( dirname( __FILE__ ) . '/../local-config.php' ) ) {
-    # define( 'WP_LOCAL_DEV', true );
-    define( 'WPLT_SERVER', 'dev' );
-    include( dirname( __FILE__ ) . '/../local-config.php' );
 } else {
     define('DB_NAME', 'prod_db_name');
     define('DB_USER', 'prod_db_user');
